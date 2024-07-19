2de405406dcc8a027d778eeb393c859a9abca24c
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2024-07-19 14:09:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
 import os
 from PIL import Image
 import streamlit as st
-
+from io import BytesIO
 
 def convert_and_save(uploaded_file):
     if uploaded_file is not None:
         # Open the uploaded file as an image
         img = Image.open(uploaded_file)
         rgb_img = img.convert('RGB')
-        # Construct the new file name
-        file_name = os.path.splitext(uploaded_file.name)[0] + '.jpeg'
-        # Save the image in JPEG format in the same folder as the uploaded file
-        save_path = os.path.join(os.path.dirname(uploaded_file.name), file_name)
-        rgb_img.save(save_path, 'JPEG')
-        return save_path
+        # Save the image in JPEG format to a BytesIO object
+        img_byte_arr = BytesIO()
+        rgb_img.save(img_byte_arr, format='JPEG')
+        img_byte_arr.seek(0)
+        return img_byte_arr
     return None
 
-
 def main():
-    st.title("PNG to JPEG Converter")
+    st.title("PNG to JPEG Converter file")
 
     # File uploader for multiple PNG files
     uploaded_files = st.file_uploader("Choose PNG files", type="png", accept_multiple_files=True)
 
     if st.button("Convert to JPEG"):
         for uploaded_file in uploaded_files:
-            save_path = convert_and_save(uploaded_file)
-            if save_path:
-                st.success(f"Converted and saved to {save_path}")
+            img_byte_arr = convert_and_save(uploaded_file)
+            if img_byte_arr:
+                file_name = os.path.splitext(uploaded_file.name)[0] + '.jpeg'
+                st.download_button(
+                    label=f"Download {file_name}",
+                    data=img_byte_arr,
+                    file_name=file_name,
+                    mime='image/jpeg'
+                )
+                st.success(f"Converted and ready for download: {file_name}")
             else:
                 st.error("Failed to convert the file")
-
 
 if __name__ == "__main__":
     main()
